84c968350ed57fec06597da03489c15f8239b718
Noa Weiss <noa67weiss@gmail.com>
2026-05-04 11:30:17 +0300

    task 2 - no loop

--- Looper.c
+++ Looper.c
@@ -9,28 +9,26 @@ void handler(int sig)
     printf("\nRecieved Signal : %s\n", strsignal(sig));
     fflush(stdout); // to make sure the message is printed
 
-    signal(sig, SIG_DFL); // restore default handler for the signal
-    raise(sig);           // re send the signal to actally do it
-
     if (sig == SIGTSTP) // will frezze the process
     {
-        signal(SIGTSTP, handler);
         signal(SIGCONT, handler);
     }
     else if (sig == SIGCONT) // will wake up
     {
         signal(SIGTSTP, handler);
-        signal(SIGCONT, handler);
     }
+    signal(sig, SIG_DFL); // restore default handler for the signal
+    raise(sig);           // re send the signal to actally do it
+    return;
 }
 
 int main(int argc, char **argv)
 {
     printf("Starting the program\n");
     // the signals the progran should handle!
-    signal(SIGINT, handler);
+    // signal(SIGINT, handler);
     signal(SIGTSTP, handler);
-    signal(SIGCONT, handler);
+    // signal(SIGCONT, handler);
 
     while (1)
     {
